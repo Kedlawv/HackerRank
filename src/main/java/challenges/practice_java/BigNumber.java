0c0098ef164311b89cdf5b273dbd23f39a970275
Kedlawv <waldek.bus@gmail.com>
2020-02-09 18:01:46 +0100

    add and multiply BigInteger

--- src/main/java/challenges/practice_java/BigNumber.java
+++ src/main/java/challenges/practice_java/BigNumber.java
@@ -4,9 +4,15 @@ import java.math.BigInteger;
 
 public class BigNumber {
 
-    public static boolean primalityTest(String n){
+    public static boolean primalityTest(String n) {
         return new BigInteger(n).isProbablePrime(1);
     }
 
-    
+    public static BigInteger[] addAndMultiplyBigInt(String a, String b) {
+        BigInteger aBI = new BigInteger(a);
+        BigInteger bBI = new BigInteger(b);
+        BigInteger[] result = {aBI.add(bBI), aBI.multiply(bBI)};
+
+        return result;
+    }
 }
--- src/test/java/challenges/practice_java/BigNumberTest.java
+++ src/test/java/challenges/practice_java/BigNumberTest.java
@@ -4,20 +4,33 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.math.BigInteger;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BigNumberTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"2","3","5","7","13"})
+    @ValueSource(strings = {"2", "3", "5", "7", "13"})
     void primalityTestTestTrueForPrime(String prime) {
         assertTrue(BigNumber.primalityTest(prime));
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"4","6","8","9","14"})
+    @ValueSource(strings = {"4", "6", "8", "9", "14"})
     void primalityTestTestTrueForNotPrime(String notPrime) {
         assertFalse(BigNumber.primalityTest(notPrime));
+    }
+
+    @Test
+    public void addAndMultiplyBigIntTest() {
+        String a = "12345";
+        String b = "67";
+
+        BigInteger[] expected = {new BigInteger("12412"), new BigInteger("827115")};
+        BigInteger[] actual = BigNumber.addAndMultiplyBigInt(a, b);
+
+        assertArrayEquals(expected, actual);
     }
 
 }
