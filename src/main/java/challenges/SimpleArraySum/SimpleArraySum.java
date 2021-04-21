58bd8b4318b8c779405cf9179d0f342a7d7aa02b
Kedlawv <waldek.bus@gmail.com>
2021-04-21 19:09:12 +0200

    Simple Array Sum using Stream.reduce() and Stream.sum()

--- src/main/java/challenges/SimpleArraySum/SimpleArraySum.java
+++ src/main/java/challenges/SimpleArraySum/SimpleArraySum.java
@@ -1,5 +1,7 @@
 package challenges.SimpleArraySum;
 
+import java.util.Arrays;
+
 public class SimpleArraySum {
 
     int simpleArraySum(int[] ar) {
@@ -11,4 +13,13 @@ public class SimpleArraySum {
         }
         return sum;
     }
+
+    int simpleArraySumStreamReduce(int[] ar) {
+        return Arrays.stream(ar).reduce(0, (a, b) -> a + b);
+    }
+
+    int simpleArraySumStreamSum(int[] ar){
+        return Arrays.stream(ar).sum();
+    }
+
 }
--- src/test/java/challenges/SimpleArraySum/SimpleArraySumTest.java
+++ src/test/java/challenges/SimpleArraySum/SimpleArraySumTest.java
@@ -17,4 +17,28 @@ class SimpleArraySumTest {
         assertEquals(expected, actual, "Sum of the integers is incorrect");
 
     }
+
+    @Test
+    void simpleArraySumStreamReduce() {
+        int[] inputArr = {1,2,3};
+        int expected = 6;
+
+        SimpleArraySum simpleArraySum = new SimpleArraySum();
+        int actual = simpleArraySum.simpleArraySumStreamSum(inputArr);
+
+        assertEquals(expected, actual, "Sum of the integers is incorrect");
+
+    }
+
+    @Test
+    void simpleArraySumStreamSum() {
+        int[] inputArr = {1,2,3};
+        int expected = 6;
+
+        SimpleArraySum simpleArraySum = new SimpleArraySum();
+        int actual = simpleArraySum.simpleArraySumStreamSum(inputArr);
+
+        assertEquals(expected, actual, "Sum of the integers is incorrect");
+
+    }
 }
