e17ebecc750d941cb18ab7579162957c471a0a6f
Kedlawv <waldek.bus@gmail.com>
2021-05-25 19:36:43 +0200

    removed isSorted() check and break in nested loop after finding correct value index. All good, 10000 values test past under time limit

--- src/main/java/challenges/MinimumSwaps.java
+++ src/main/java/challenges/MinimumSwaps.java
@@ -9,34 +9,23 @@ public class MinimumSwaps {
 
     public static int minimumSwaps(int[] arr) {
         int numSwaps = 0;
-        boolean sorted = checkIfSorted(arr);
-
-        while (!sorted) {
-            for (int i = 0; i < arr.length; i++) {
-                if (arr[i] != i + 1) {
-                    for (int j = i + 1; j < arr.length; j++) {
-                        if (arr[j] == i + 1) {
-                            int temp = arr[i];
-                            arr[i] = arr[j];
-                            arr[j] = temp;
-                            numSwaps++;
-                        }
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != i + 1) {
+                for (int j = i + 1; j < arr.length; j++) {
+                    if (arr[j] == i + 1) {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                        numSwaps++;
+                        break;
                     }
                 }
             }
-            sorted = checkIfSorted(arr);
         }
+
 
         return numSwaps;
     }
 
-    private static boolean checkIfSorted(int[] arr) {
-        boolean sorted;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != i + 1) {
-                return false;
-            }
-        }
-        return true;
-    }
 }
